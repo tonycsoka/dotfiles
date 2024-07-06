07f77aa8a8ed3b699151e784e4c1720c3caf858d
Tony Csoka <Tony.Csoka@gmail.com>
2024-07-06 16:54:00 +0100

    add commentary

--- .config/nvim/lua/jackdaw/keymap.lua
+++ .config/nvim/lua/jackdaw/keymap.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-
+vim.keymap.set("x", "<leader>/", ":Commentary<cr>", { remap = true })
+vim.keymap.set("v", "<leader>/", ":Commentary<cr>", { remap = true })
+vim.keymap.set("n", "<leader>/", "<cmd>Commentary<cr>", { remap = true })
