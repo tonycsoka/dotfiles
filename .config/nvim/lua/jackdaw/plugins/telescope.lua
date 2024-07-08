1cb601b1014e0beed54bd780e75324c20c35eb28
Tony Csoka <Tony.Csoka@gmail.com>
2024-07-08 07:28:57 +0100

    telescope keymaps

--- .config/nvim/lua/jackdaw/plugins/telescope.lua
+++ .config/nvim/lua/jackdaw/plugins/telescope.lua
@@ -14,6 +14,7 @@ return {
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+		vim.keymap.set("n", "<leader>ft", builtin.live_grep, {})
 		vim.keymap.set("n", "<leader>fw", function()
 			local word = vim.fn.expand("<cword>")
 			builtin.grep_string({ search = word })
@@ -22,9 +23,6 @@ return {
 			local word = vim.fn.expand("<cWORD>")
 			builtin.grep_string({ search = word })
 		end)
-		vim.keymap.set("n", "<leader>ft", function()
-			builtin.grep_string({ search = vim.fn.input("Grep > ") })
-		end)
-		vim.keymap.set("n", "<leader>h", builtin.help_tags, {})
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 	end,
 }
