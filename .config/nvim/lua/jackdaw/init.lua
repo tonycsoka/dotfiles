e8dfd74119c679716ff6c8059e992c3d7e06c7d7
Tony Csoka <Tony.Csoka@gmail.com>
2024-07-11 18:18:47 +0100

    folding defaults

--- .config/nvim/lua/jackdaw/init.lua
+++ .config/nvim/lua/jackdaw/init.lua
@@ -37,7 +37,7 @@ require("jackdaw.lazy")
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldcolumn = "0"
--- vim.opt.foldtext = ""
+vim.opt.foldtext = ""
 -- vim.opt.foldlevel = 99
 -- vim.opt.foldlevelstart = 1
 -- vim.opt.foldnestmax = 4
