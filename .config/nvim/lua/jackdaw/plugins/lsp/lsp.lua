24437ff54327950a12955d48642c77b896cc209d
Tony Csoka <Tony.Csoka@gmail.com>
2024-07-12 10:54:54 +0100

    folding

--- .config/nvim/lua/jackdaw/plugins/lsp/lsp.lua
+++ .config/nvim/lua/jackdaw/plugins/lsp/lsp.lua
@@ -8,6 +8,7 @@ return {
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-cmdline",
 		"hrsh7th/nvim-cmp",
+    "L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip",
 		"j-hui/fidget.nvim",
     "onsails/lspkind.nvim",
