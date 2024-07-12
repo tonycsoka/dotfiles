c6c221de489edaa53eca8f7a125cb10055172853
Tony Csoka <Tony.Csoka@gmail.com>
2024-07-12 10:12:32 +0100

    folding

--- .config/nvim/lua/jackdaw/init.lua
+++ .config/nvim/lua/jackdaw/init.lua
@@ -38,8 +38,8 @@ vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldcolumn = "0"
 vim.opt.foldtext = ""
--- vim.opt.foldlevel = 99
--- vim.opt.foldlevelstart = 1
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 2
 -- vim.opt.foldnestmax = 4
 
 vim.cmd.colorscheme("catppuccin")
