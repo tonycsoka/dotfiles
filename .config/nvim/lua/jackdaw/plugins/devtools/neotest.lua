de85d2398965e97d141be75f81285762740badd2
Tony Csoka <Tony.Csoka@gmail.com>
2024-07-20 02:45:48 +0100

    add keybind for test summary

--- .config/nvim/lua/jackdaw/plugins/devtools/neotest.lua
+++ .config/nvim/lua/jackdaw/plugins/devtools/neotest.lua
@@ -2,15 +2,23 @@ require("jackdaw.postfix").add(function()
 	vim.keymap.set("n", "<leader>dt", function()
 		require("neotest").run.run()
 	end, { desc = "Run nearest test" })
+
 	vim.keymap.set("n", "<leader>dT", function()
 		require("neotest").run.run({ strategy = "dap" }) ---@diagnostic disable-line: missing-fields
 	end, { desc = "Debug nearest test" })
+
 	vim.keymap.set("n", "<leader>df", function()
 		require("neotest").run.run()
 	end, { desc = "Run all tests" })
+
 	vim.keymap.set("n", "<leader>dF", function()
 		require("neotest").run.run({ vim.fn.expand("%"), strategy = "dap" }) ---@diagnostic disable-line: missing-fields
 	end, { desc = "Debug all tests" })
+
+	vim.keymap.set("n", "<leader>ds", function()
+		require("neotest").summary.toggle() ---@diagnostic disable-line: missing-fields
+	end, { desc = "Test Summary Toggle" })
+
 	require("neotest").setup({ ---@diagnostic disable-line: missing-fields
 		adapters = {
 			require("neotest-python"),
