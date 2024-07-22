5d60d9409271f21a2edf62f9b9421b0613c9a391
Tony Csoka <Tony.Csoka@gmail.com>
2024-07-22 08:43:53 +0100

    keymaps

--- .config/nvim/lua/jackdaw/plugins/telescope.lua
+++ .config/nvim/lua/jackdaw/plugins/telescope.lua
@@ -11,18 +11,18 @@ return {
 		require("telescope").setup({}) ---@diagnostic disable-line: redundant-parameter
 
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-		vim.keymap.set("n", "<leader>ft", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope Find File" })
+		vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Telescope Find Git Files" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope Find Buffer" })
+		vim.keymap.set("n", "<leader>ft", builtin.live_grep, { desc = "Telescope Grep" })
 		vim.keymap.set("n", "<leader>fw", function()
 			local word = vim.fn.expand("<cword>")
 			builtin.grep_string({ search = word })
-		end)
+		end, { desc = "Telescope Find word" })
 		vim.keymap.set("n", "<leader>fW", function()
 			local word = vim.fn.expand("<cWORD>")
 			builtin.grep_string({ search = word })
-		end)
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+		end, { desc = "Telescope Find wORD" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope Help Tags" })
 	end,
 }
