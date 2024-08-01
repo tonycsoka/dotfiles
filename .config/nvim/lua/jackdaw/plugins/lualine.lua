05dc3cde1d8e95056880eb273732d638baea1500
Tony Csoka <Tony.Csoka@gmail.com>
2024-08-01 09:30:38 +0100

    tidy up lualine a bit

--- .config/nvim/lua/jackdaw/plugins/lualine.lua
+++ .config/nvim/lua/jackdaw/plugins/lualine.lua
@@ -117,15 +117,15 @@ return {
 				},
 				sections = {
 					lualine_a = {
-						-- "mode",
 						{
 							require("noice").api.status.command.get,
 							cond = require("noice").api.status.command.has,
 						},
-						{
-							require("noice").api.status.mode.get,
-							cond = require("noice").api.status.mode.has,
-						},
+						"mode",
+						-- {
+						-- 	require("noice").api.status.mode.get,
+						-- 	cond = require("noice").api.status.mode.has,
+						-- },
 						{
 							require("noice").api.status.search.get,
 							cond = require("noice").api.status.search.has,
