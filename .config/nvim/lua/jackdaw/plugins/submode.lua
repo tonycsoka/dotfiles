1a8a0dc0fa16b7bded9672a1a8da60f13e41b47f
Tony Csoka <Tony.Csoka@gmail.com>
2024-09-02 15:15:42 +0100

    submode

--- .config/nvim/lua/jackdaw/plugins/submode.lua
+++ .config/nvim/lua/jackdaw/plugins/submode.lua
@@ -39,6 +39,96 @@ require("jackdaw.postfix").add(function()
 			},
 		},
 	})
+
+	local debug_mode = layers.mode.new()
+	debug_mode:auto_show_help()
+
+	vim.keymap.set("n", "<leader>dd", function()
+		debug_mode.window.config.title = "Debug Mode"
+		debug_mode:activate()
+	end, { desc = "Refactor Mode" })
+
+	debug_mode:keymaps({
+		n = {
+			{
+				"c",
+				function()
+					require("dap").continue()
+				end,
+				{ desc = "Continue" },
+			},
+			{
+				"b",
+				function()
+					require("dap").toggle_breakpoint()
+				end,
+				{ desc = "Toggle breakpoint" },
+			},
+			{
+				"i",
+				function()
+					require("dap").step_into()
+				end,
+				{ desc = "DAP Step into" },
+			},
+			{
+				"o",
+				function()
+					require("dap").step_over()
+				end,
+				{ desc = "DAP Step over" },
+			},
+			{
+				"u",
+				function()
+					require("dapui").toggle()
+				end,
+				{ desc = "DAP Toggle UI" },
+			},
+			{
+				"t",
+				function()
+					require("neotest").run.run()
+				end,
+				{ desc = "Run nearest test" },
+			},
+			{
+				"T",
+				function()
+					require("neotest").run.run({ strategy = "dap" }) ---@diagnostic disable-line: missing-fields
+				end,
+				{ desc = "Debug nearest test" },
+			},
+			{
+				"f",
+				function()
+					require("neotest").run.run()
+				end,
+				{ desc = "Run all tests" },
+			},
+			{
+				"F",
+				function()
+					require("neotest").run.run({ vim.fn.expand("%"), strategy = "dap" }) ---@diagnostic disable-line: missing-fields
+				end,
+				{ desc = "Debug all tests" },
+			},
+			{
+				"s",
+				function()
+					require("neotest").summary.toggle() ---@diagnostic disable-line: missing-fields
+				end,
+				{ desc = "Test Summary Toggle" },
+			},
+			{
+				"q",
+				function()
+					debug_mode:deactivate()
+				end,
+				{ desc = "quit" },
+			},
+		},
+	})
 end)
 
 return {
