ddaf9947644172a3ec678621fada21ed1d49afac
Tony Csoka <Tony.Csoka@gmail.com>
2024-12-09 06:03:13 +0000

    s

--- .config/nvim-global/lua/jackdaw/plugins/octo.lua
+++ .config/nvim-global/lua/jackdaw/plugins/octo.lua
@@ -1,12 +1,14 @@
 return {
-	"pwntester/octo.nvim",
-	requires = {
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope.nvim",
-		-- OR 'ibhagwan/fzf-lua',
-		"nvim-tree/nvim-web-devicons",
+	{
+		"pwntester/octo.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			-- OR 'ibhagwan/fzf-lua',
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("octo").setup()
+		end,
 	},
-	config = function()
-		require("octo").setup()
-	end,
 }
