e9d228dc5b2865adb98306f54c595789525f6a21
Tony Csoka <Tony.Csoka@gmail.com>
2024-12-15 10:37:51 +0000

    pull in gherkin treesitter grammar

--- .config/nvim-global/lua/jackdaw/plugins/treesiter.lua
+++ .config/nvim-global/lua/jackdaw/plugins/treesiter.lua
@@ -4,6 +4,18 @@ return {
 		event = { "BufReadPre", "BufNewFile" },
 		build = ":TSUpdate",
 		config = function()
+			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+			parser_config.gherkin = {
+				install_info = {
+					url = "https://github.com/tonycsoka/tree-sitter-gherkin", -- local path or git repo
+					files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+					-- optional entries:
+					branch = "main", -- default branch in case of git repo if different from master
+					generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+					requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+				},
+				filetype = "cucumber", -- if filetype does not match the parser name
+			}
 			require("nvim-treesitter.configs").setup({ ---@diagnostic disable-line: missing-fields
 				-- A list of parser names, or "all"
 				ensure_installed = {
