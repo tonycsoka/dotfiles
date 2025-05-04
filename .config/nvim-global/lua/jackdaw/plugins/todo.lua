79b96817c7945d970961ee243f19214a97331dff
Tony Csoka <Tony.Csoka@gmail.com>
2025-05-04 11:31:45 +0100

    todo keymaps

--- .config/nvim-global/lua/jackdaw/plugins/todo.lua
+++ .config/nvim-global/lua/jackdaw/plugins/todo.lua
@@ -3,5 +3,35 @@ return {
 		"folke/todo-comments.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = {},
+		keys = {
+			{
+				"<leader>st",
+				function()
+					Snacks.picker.todo_comments()
+				end,
+				desc = "Todo",
+			},
+			{
+				"<leader>sT",
+				function()
+					Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
+				end,
+				desc = "Todo/Fix/Fixme",
+			},
+			{
+				"]t",
+				function()
+					require("todo-comments").jump_next()
+				end,
+				desc = "Next todo comment",
+			},
+			{
+				"[t",
+				function()
+					require("todo-comments").jump_prev()
+				end,
+				desc = "Previous todo comment",
+			},
+		},
 	},
 }
