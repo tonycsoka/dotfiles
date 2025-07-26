d0f58a63fe382fed448419961056f20d662351ec
Tony Csoka <tony.csoka@gmail.com>
2025-07-26 10:56:10 +0100

    wall setter

--- wall.sh
+++ wall.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 cp $1 ~/.config/wallpaper.jpg
-wal --saturate 0.25 -e -q -i ~/.config/wallpaper.jpg
+wal --contrast 1.0 --saturate 0.25 -n -e -q -i ~/.config/wallpaper.jpg
 
 source ~/.cache/wal/colors.sh
 
 sed -i "s/\( active-color\).*/\1 \"$color6\"/" ~/.config/niri/config.kdl
+systemctl --user restart swaybg
