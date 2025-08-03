74f853788f5887e9f710b0fcf9cbf0219471f840
Tony Csoka <Tony.Csoka@gmail.com>
2025-08-03 12:12:32 +0100

    refactor

--- .config/nvim/lua/jackdaw/init.lua
+++ .config/nvim/lua/jackdaw/init.lua
@@ -5,7 +5,4 @@ require("jackdaw.diagnostics")
 require("jackdaw.commands")
 require("jackdaw.test")
 
-vim.cmd.colorscheme("catppuccin")
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none", ctermbg = "none" })
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none", ctermbg = "none" })
-vim.api.nvim_set_hl(0, "FloatTitle", { bg = "none", ctermbg = "none" })
+require("jackdaw.theme")
---
+++ .config/nvim/lua/jackdaw/theme.lua
@@ -0,0 +1,4 @@
+vim.cmd.colorscheme("catppuccin")
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none", ctermbg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none", ctermbg = "none" })
+vim.api.nvim_set_hl(0, "FloatTitle", { bg = "none", ctermbg = "none" })
