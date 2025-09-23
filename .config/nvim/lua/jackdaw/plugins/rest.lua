d4345b062a5477f516e1c649f1621d62891a4c4c
Tony Csoka <Tony.Csoka@gmail.com>
2025-09-23 06:47:55 +0100

    rest

--- .config/nvim/lua/jackdaw/plugins/rest.lua
+++ .config/nvim/lua/jackdaw/plugins/rest.lua
@@ -1,1 +1,17 @@
-return {}
+return {
+	{
+		"mistweaverco/kulala.nvim",
+		keys = {
+			{ "<leader>ks", desc = "Send request" },
+			{ "<leader>ka", desc = "Send all requests" },
+			{ "<leader>kb", desc = "Open scratchpad" },
+		},
+		ft = { "http", "rest" },
+		opts = {
+			-- your configuration comes here
+			global_keymaps = true,
+			global_keymaps_prefix = "<leader>k",
+			kulala_keymaps_prefix = "",
+		},
+	},
+}
