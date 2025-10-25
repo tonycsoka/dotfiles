89029f23b2887396d4b00d0cd1187132efd4c4d4
Tony Csoka <tony.csoka@gmail.com>
2025-10-25 11:03:38 +0100

    add other langs

--- .config/nvim/lua/jackdaw/plugins/devtools/dap.lua
+++ .config/nvim/lua/jackdaw/plugins/devtools/dap.lua
@@ -43,23 +43,25 @@ return {
 				},
 			}
 
-			dap.configurations.typescriptreact = {
-				{
-					type = "pwa-node",
-					request = "launch",
-					name = "Debug Jest Tests",
-					-- trace = true, -- include debugger info
-					runtimeExecutable = "node",
-					runtimeArgs = {
-						"./node_modules/jest/bin/jest.js",
-						"--runInBand",
+			for _, language in ipairs({ "typescriptreact", "javascriptreact", "typescript", "javascript" }) do
+				dap.configurations[language] = {
+					{
+						type = "pwa-node",
+						request = "launch",
+						name = "Debug Jest Tests",
+						-- trace = true, -- include debugger info
+						runtimeExecutable = "node",
+						runtimeArgs = {
+							"./node_modules/jest/bin/jest.js",
+							"--runInBand",
+						},
+						rootPath = "${workspaceFolder}",
+						cwd = "${workspaceFolder}/src",
+						console = "integratedTerminal",
+						internalConsoleOptions = "neverOpen",
 					},
-					rootPath = "${workspaceFolder}",
-					cwd = "${workspaceFolder}/src",
-					console = "integratedTerminal",
-					internalConsoleOptions = "neverOpen",
-				},
-			}
+				}
+			end
 		end,
 	},
 	{
