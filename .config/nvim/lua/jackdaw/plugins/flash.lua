f2585df505ae12989c4c0130e6dc10be8dbfe1ca
Tony Csoka <Tony.Csoka@gmail.com>
2025-11-08 07:40:23 +0000

    flash

--- .config/nvim/lua/jackdaw/plugins/flash.lua
+++ .config/nvim/lua/jackdaw/plugins/flash.lua
@@ -5,10 +5,10 @@ return {
 	opts = {},
   -- stylua: ignore
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "flash treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "remote flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "treesitter search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
 }
